f1236e00fa2f49214f99cce90901432b22b5c2b9
Guilherme <guiescsal@gmail.com>
2025-02-14 13:50:35 -0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import Home from './screens/Home';
 import Earn from './screens/Earn';
-import AirDrop from './screens/AirDrop';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import BottomNavigation from './components/BottomNavigation';
 
